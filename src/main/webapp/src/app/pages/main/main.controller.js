bc2461c64736c60c2c1d43cb1029bacb219ed8c3
Edwin Munguia <daiokaio@gmail.com>
2016-10-22 18:51:18 -0500

    added major button links

--- src/main/webapp/src/app/pages/main/main.controller.js
+++ src/main/webapp/src/app/pages/main/main.controller.js
@@ -1,10 +1,19 @@
 'use strict';
 
-function MainController($log) {
+function MainController($log, $state) {
   'ngInject';
   
   this.logger = function() {
     $log.log('heyyy');
+  };
+
+  this.mainCategoryLink = function(type){
+    switch(type){
+      case 1: $state.go('prevention'); break;
+      case 2: $state.go('emergency'); break;
+      case 3: $state.go('continuous'); break;
+      default: $state.go('contact'); break; //<-- We should not be here, but eh.
+    }
   };
 
 }
