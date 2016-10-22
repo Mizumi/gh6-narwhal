cc73bba64b4350d8230a5a592c800adc0d09496f
epixal <joshuagagen@outlook.com>
2016-10-22 18:10:16 -0500

    get all services for coc

--- src/main/java/io/alicorn/server/http/ServiceEndpoint.java
+++ src/main/java/io/alicorn/server/http/ServiceEndpoint.java
@@ -1,13 +1,80 @@
 package io.alicorn.server.http;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.alicorn.data.jongothings.ServiceDbFacade;
+import io.alicorn.data.models.services.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class ServiceEndpoint {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceEndpoint.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Inject
-    public ServiceEndpoint() {
+    public ServiceEndpoint(SparkWrapper spark, ServiceDbFacade serviceDbFacade, LoginEndpoint loginEndpoint) {
+
+        // Create / Update
+        spark.post("/api/services", (req, res) -> {
+            if (loginEndpoint.isCurrentUserAgent()) {
+                Service service = objectMapper.readValue(
+                        Json.parse(req.body()).asObject().get("service").toString(),
+                        Service.class);
+
+                serviceDbFacade.setService(service);
+                return new WebserviceResponse().toString();
+            } else {
+                return new WebserviceResponse().addError("Current user is not authorized to create and/or update a service");
+            }
+        });
+
+        // Retrieve
+        spark.get("/api/services/:uuid", (req, res) -> {
+            Service service = serviceDbFacade.getService(req.params(":uuid"));
+            if (service != null) {
+                return new WebserviceResponse().set("service", Json.parse(objectMapper.writeValueAsString(service)));
+            } else {
+                return new WebserviceResponse().addError("No continuum exists with UUID: " + req.params(":uuid"));
+            }
+        });
+
+        // Delete
+        spark.post("/api/service/delete", (req, res) -> {
+            String uuidToDelete = Json.parse(req.body()).asObject().get("uuid").asString();
+            if (loginEndpoint.isCurrentUserAgent()) {
+                serviceDbFacade.deleteService(uuidToDelete);
+                return new WebserviceResponse().toString();
+            }
+            return new WebserviceResponse().addError("Ah ah ah! You didn't say the magic word.");
+        });
+
+        // Get all for continuum
+        spark.post("/api/service/getServicesForCoc", (req, res) -> {
+            StringBuilder sb = new StringBuilder().append("[ ");
+            JsonArray uuids = Json.parse(req.body()).asObject().get("uuids").asArray();
+            for (int i = 0; i < uuids.size(); i++) {
+                sb.append(uuids.get(i));
+                if (i < uuids.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append(" ]");
+
+            JsonArray services = new JsonArray();
+            serviceDbFacade.getAllInList(sb.toString()).forEachRemaining((service -> {
+                try {
+                    services.add(Json.parse(objectMapper.writeValueAsString(service)));
+                } catch (Exception ex) {
+                    logger.warn(ex.getMessage());
+                }
+            }));
+            return new WebserviceResponse().set("services", services).toString();
+        });
 
     }
 }
