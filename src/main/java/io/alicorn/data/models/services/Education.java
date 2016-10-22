4071ddfe02fc05e1f73f8bf7e8d09484c8ead1f8
epixal <joshuagagen@outlook.com>
2016-10-22 11:08:32 -0500

    yup

--- src/main/java/io/alicorn/data/models/services/Education.java
+++ src/main/java/io/alicorn/data/models/services/Education.java
@@ -1,8 +1,11 @@
 package io.alicorn.data.models.services;
 
 
+import io.alicorn.data.models.ContactInfo;
+
 public class Education extends Service {
     public Education() {
         super(ServiceType.Education);
     }
+
 }
